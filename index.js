207a6b7246d3652e7a8f1d152a29cfd30cbc9b21
karthikeyakannaiah <50999627+karthikeyakannaiah@users.noreply.github.com>
2020-06-10 22:09:33 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,10 @@
+
+function lgnm(){
+	document.getElementById("lgnm").style.display = "none";
+    document.getElementById("lgn").style.display = "block";
+}
+console.log("working")
+function sgnp(){
+	document.getElementById("sgnp").style.display = "none";
+    document.getElementById("sgn").style.display = "block";
+}
